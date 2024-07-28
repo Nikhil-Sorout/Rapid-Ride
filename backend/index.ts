9264b57b45fe93e1d568f868addc1a02715006c5
Nikhil <nikhilsorout6@gmail.com>
2024-07-28 17:41:50 +0530

    creating the driver and passenger app

--- backend/index.ts
+++ backend/index.ts
@@ -15,12 +15,20 @@ const pusher = new Pusher({
     secret:  process.env.APP_SECRET || '',
     cluster: process.env.APP_CLUSTER || '',
 })
+pusher.trigger("private-ride-channel", "my-event", { message: "hello world" });
 
 app.get('/', async(req: express.Request, res: express.Response)=>{
     res.json({message: "Hello from the server"})
     // console.log(res);
 })
 
+app.post('/trigger-event', (req, res)=>{
+    console.log(req.body);
+    const {channel, message, event} = req.body;
+
+    // pusher.trigger(channel, 'message', message);
+    res.sendStatus(200);
+})
 // for authenticating users
 app.get('/pusher/auth', async(req: express.Request, res: express.Response)=>{
     const query = req.query;
